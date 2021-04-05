3076025163d52d82c8d68f804f2deec49b05b58c
Davi Alvarenga <daavigiroux@gmail.com>
2021-04-05 19:19:12 -0400

    Changing production api endpoint

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `fill me in when we deploy`;
+export const prodEndpoint = `https://loja-refugio-api.herokuapp.com/api/graphql`;
 export const porPagina = 6;
