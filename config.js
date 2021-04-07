2fb4db8d3a8ae1bde110d7573140a61c542f3fbf
Davi Alvarenga <daavigiroux@gmail.com>
2021-04-07 16:15:19 -0400

    Changing back old endpoint until new one is ready

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = 'https://admin.lojarefugio.com.br/api/graphql';
+export const prodEndpoint =
+  'https://loja-refugio-api.herokuapp.com/api/graphql';
 export const porPagina = 6;
