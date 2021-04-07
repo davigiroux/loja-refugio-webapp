57cb00ce17ad086beb8341fe118b5f3ca5019c08
Davi Alvarenga <daavigiroux@gmail.com>
2021-04-07 19:41:51 -0400

    document change

--- pages/_document.js
+++ pages/_document.js
@@ -34,7 +34,7 @@ export default class MyDocument extends Document {
       <Html lang="pt-Br">
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap"
             rel="stylesheet"
