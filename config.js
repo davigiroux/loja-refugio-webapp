ca9e82e0defc4bd1c393676c3b9b2a4ee899f823
Davi Alvarenga <daavigiroux@gmail.com>
2021-04-19 17:52:31 -0400

    Changing items per page in store

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const endpoint = `http://localhost:3000/api/graphql`;
 export const prodEndpoint = 'https://admin.lojarefugio.com.br/api/graphql';
-export const porPagina = 6;
+export const porPagina = 9;
